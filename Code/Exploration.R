0a970189ecc7d9b7e891b7eab361f21f42f1d18f
adri-tab <adrien.tableau@ofb.gouv.fr>
2021-11-30 14:57:16 +0100

    cleaning

--- Code/Exploration.R
+++ Code/Exploration.R
@@ -31,7 +31,7 @@ l1 %>%
          across(c(LONG_DEG, LONG_MIN, LONG_SEC, LAT_DEG, LAT_MIN, LAT_SEC), 
                 ~ .x %>% 
                   str_replace(",|:|;", ".") %>%
-                  as.numeric()), 
+                  as.numeric()),
          lon = if_else(LONG_DEG < 0, 
                         LONG_DEG - LONG_MIN / 60 - LONG_SEC / 60^2, 
                         LONG_DEG + LONG_MIN / 60 + LONG_SEC / 60^2),
@@ -56,7 +56,7 @@ l1 %>%
            TRUE ~ "ind")) %>% 
   select(obs = ACTION, datetime, ring = BAGUE,
          sp = ESPECE, sex, age, 
-         lon, lat, co = PAYS, com = COMMUNE, dpt = DEPARTEMENT, spot = LIEUDIT, 
+         lon, lat, co = PAYS, dpt = DEPARTEMENT, com = COMMUNE, spot = LIEUDIT, 
          raw_lon, raw_lat, 
          `CODE MARQUE`, `DOUBLE MARQUAGE`, `COULEUR MARQUE`,
          MODE, CENTRE, BAGUEUR, `N°PROGRAMME`, `BAGUE RECOMPENSE`, FREQUENCE_EMETTEUR, 
@@ -69,21 +69,18 @@ l1 %>%
   rowid_to_column() -> l2[[1]] 
 
 # t_controle
-
 l1 %>% 
-  pluck(2) %>% filter(is.na(`CODE MARQUE`), is.na(BAGUE))
-  filter()
-  count(ESPECE)
-  filter(ESPECE %in% c("AYTFER", "AYTFUL"), # only 2 species
-         DEPARTEMENT %in% c("44", "53", "01", "77", "89", "41", "42")) %>% # only main dpt
+  pluck(2) %>% 
+  filter(!is.na(BAGUE), 
+         ESPECE %in% c("AYTFER", "AYTFUL")) %>% 
   mutate(across(HEURE, ~ if_else(is.na(HEURE), ymd_hms(20000101120000), HEURE)),
          datetime = str_c(date(DATE), "T", hour(HEURE), ":", minute(HEURE)) %>% 
            ymd_hm(tz = "Europe/Paris"),
-         across(ACTION, ~ if_else(.x == "BAGUAGE", .x, "CONTROLE")),
+         obs = "CONTROLE",
          across(c(LONG_DEG, LONG_MIN, LONG_SEC, LAT_DEG, LAT_MIN, LAT_SEC), 
                 ~ .x %>% 
                   str_replace(",|:|;", ".") %>%
-                  as.numeric()), 
+                  as.numeric()),
          lon = if_else(LONG_DEG < 0, 
                        LONG_DEG - LONG_MIN / 60 - LONG_SEC / 60^2, 
                        LONG_DEG + LONG_MIN / 60 + LONG_SEC / 60^2),
@@ -91,24 +88,10 @@ l1 %>%
                        LAT_DEG - LAT_MIN / 60 - LAT_SEC / 60^2, 
                        LAT_DEG + LAT_MIN / 60 + LAT_SEC / 60^2), 
          raw_lon = str_c(LONG_DEG, LONG_MIN, LONG_SEC, sep = ":"),
-         raw_lat = str_c(LAT_DEG, LAT_MIN, LAT_SEC, sep = ":"), 
-         sex = case_when(
-           SEXE %in% c("F", "Femelle") ~ "fem",
-           SEXE == "M" ~ "mal", 
-           is.na(SEXE) ~ NA_character_,
-           TRUE ~ "ind"), 
-         age = case_when(
-           AGE %>% str_detect("\\?") ~ "ind",
-           AGE %>% str_detect("\\+1") ~ "+1A",
-           AGE %>% str_detect("\\+") ~ "+2A",
-           AGE %>% str_starts("1|VOL") ~ "1A",
-           AGE %>% str_starts("2") ~ "2A",
-           AGE == "PUL" ~ "pul",
-           is.na(AGE) ~ NA_character_,
-           TRUE ~ "ind")) %>% 
-  select(obs = ACTION, datetime, id = BAGUE,
-         sp = ESPECE, sex, age, 
-         lon, lat, co = PAYS, com = COMMUNE, dpt = DEPARTEMENT, spot = LIEUDIT, 
+         raw_lat = str_c(LAT_DEG, LAT_MIN, LAT_SEC, sep = ":")) %>% 
+  select(obs = ACTION, datetime, ring = BAGUE,
+         sp = ESPECE, SEXE, AGE, 
+         lon, lat, co = PAYS, dpt = DEPARTEMENT, com = COMMUNE, spot = LIEUDIT, 
          raw_lon, raw_lat, 
          `CODE MARQUE`, `DOUBLE MARQUAGE`, `COULEUR MARQUE`,
          MODE, CENTRE, BAGUEUR, `N°PROGRAMME`, `BAGUE RECOMPENSE`, FREQUENCE_EMETTEUR, 
@@ -117,4 +100,4 @@ l1 %>%
          PARASITES, `PRISE DE SANG`, `PRELEVEMENT PLUME`, 
          `PRELEVEMENT CLOACAL`, `BUT PREL CLOACAL`, `N°ECOUVILLON`, 
          DATE_IMPORT, NUMERO_IMPORT, `REMARQUES`) %>% 
-  arrange(obs, datetime, sp, id) -> l1[[1]] 
+  arrange(obs, datetime, sp, ring) -> l2[[2]] 
