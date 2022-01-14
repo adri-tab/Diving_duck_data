796d5fc2d002d9cee8d3f60d752ad30a38a9c99a
adri-tab <adrien.tableau@ofb.gouv.fr>
2022-01-14 17:12:31 +0100

    country match test

--- Code/Exploration.R
+++ Code/Exploration.R
@@ -5,11 +5,14 @@ require(tidyverse)
 require(readxl)
 require(lubridate)
 require(sf)
+library(leaflet)
+library(htmlwidgets)
 
+options(viewer = NULL) 
 
 # Importation -----------------------------------------------------------------------
 
-c("t_capture", "t_controle", "t_controle_coo", "t_reprise") %>% 
+c("t_capture", "t_controle", "t_controle_coo", "t_reprise", "t_commune", "t_spot") %>% 
   set_names() %>% 
   map(~ read_excel("./Data/data_duck.xlsx",
            sheet = .x, 
@@ -19,7 +22,7 @@ c("t_capture", "t_controle", "t_controle_coo", "t_reprise") %>%
 l2 <- list()
 
 
-# Nettoyage par table ---------------------------------------------------------------
+# Table cleaning  ------------------------------------------------------------------
 
 # t_capture
 l1 %>% 
@@ -31,18 +34,17 @@ l1 %>%
          datetime = str_c(date(DATE), "T", hour(HEURE), ":", minute(HEURE)) %>% 
            ymd_hm(tz = "Europe/Paris"),
          across(ACTION, ~ if_else(.x == "BAGUAGE", .x, "CONTROLE")),
+         dir_lon = if_else(LONG_DEG %>% str_detect("-"), -1, 1),
+         dir_lat = if_else(LAT_DEG %>% str_detect("-"), -1, 1),
          across(c(LONG_DEG, LONG_MIN, LONG_SEC, LAT_DEG, LAT_MIN, LAT_SEC), 
                 ~ .x %>% 
                   str_replace(",|:|;", ".") %>%
-                  as.numeric()),
-         lon = if_else(LONG_DEG < 0, 
-                        LONG_DEG - LONG_MIN / 60 - LONG_SEC / 60^2, 
-                        LONG_DEG + LONG_MIN / 60 + LONG_SEC / 60^2),
-         lat = if_else(LAT_DEG < 0, 
-                        LAT_DEG - LAT_MIN / 60 - LAT_SEC / 60^2, 
-                        LAT_DEG + LAT_MIN / 60 + LAT_SEC / 60^2), 
-         raw_lon = str_c(LONG_DEG, LONG_MIN, LONG_SEC, sep = ":"),
-         raw_lat = str_c(LAT_DEG, LAT_MIN, LAT_SEC, sep = ":"), 
+                  as.numeric() %>% 
+                  abs()),
+         lon = dir_lon * (LONG_DEG + LONG_MIN / 60 + LONG_SEC / 60^2),
+         lat = dir_lat * (LAT_DEG + LAT_MIN / 60 + LAT_SEC / 60^2), 
+         raw_lon = str_c(dir_lon, LONG_DEG, LONG_MIN, LONG_SEC, sep = ":"),
+         raw_lat = str_c(dir_lat, LAT_DEG, LAT_MIN, LAT_SEC, sep = ":"), 
          sex = case_when(
            SEXE %in% c("F", "Femelle") ~ "fem",
            SEXE == "M" ~ "mal", 
@@ -79,18 +81,17 @@ l1 %>%
          datetime = str_c(date(DATE), "T", hour(HEURE), ":", minute(HEURE)) %>% 
            ymd_hm(tz = "Europe/Paris"),
          obs = "CONTROLE",
+         dir_lon = if_else(LONG_DEG %>% str_detect("-"), -1, 1),
+         dir_lat = if_else(LAT_DEG %>% str_detect("-"), -1, 1),
          across(c(LONG_DEG, LONG_MIN, LONG_SEC, LAT_DEG, LAT_MIN, LAT_SEC), 
                 ~ .x %>% 
                   str_replace(",|:|;", ".") %>%
-                  as.numeric()),
-         lon = if_else(LONG_DEG < 0, 
-                       LONG_DEG - LONG_MIN / 60 - LONG_SEC / 60^2, 
-                       LONG_DEG + LONG_MIN / 60 + LONG_SEC / 60^2),
-         lat = if_else(LAT_DEG < 0, 
-                       LAT_DEG - LAT_MIN / 60 - LAT_SEC / 60^2, 
-                       LAT_DEG + LAT_MIN / 60 + LAT_SEC / 60^2), 
-         raw_lon = str_c(LONG_DEG, LONG_MIN, LONG_SEC, sep = ":"),
-         raw_lat = str_c(LAT_DEG, LAT_MIN, LAT_SEC, sep = ":")) %>%
+                  as.numeric() %>% 
+                  abs()),
+         lon = dir_lon * (LONG_DEG + LONG_MIN / 60 + LONG_SEC / 60^2),
+         lat = dir_lat * (LAT_DEG + LAT_MIN / 60 + LAT_SEC / 60^2), 
+         raw_lon = str_c(dir_lon, LONG_DEG, LONG_MIN, LONG_SEC, sep = ":"),
+         raw_lat = str_c(dir_lat, LAT_DEG, LAT_MIN, LAT_SEC, sep = ":")) %>%
   select(obs, datetime, ring = BAGUE,
          sp = ESPECE, 
          lon, lat, co = PAYS, dpt = DEPARTEMENT, com = COMMUNE, spot = LIEUDIT, 
@@ -114,10 +115,13 @@ l1 %>%
          datetime = str_c(date(DATE), "T", hour(HEURE), ":", minute(HEURE)) %>% 
            ymd_hm(tz = "Europe/Paris"),
          obs = "CONTROLE",
+         dir_lon = if_else(LONG_DEG %>% str_detect("-"), -1, 1),
+         dir_lat = if_else(LAT_DEG %>% str_detect("-"), -1, 1),
          across(c(LONG_DEG, LONG_MIN, LONG_SEC, LAT_DEG, LAT_MIN, LAT_SEC), 
                 ~ .x %>% 
                   str_replace(",|:|;", ".") %>%
-                  as.numeric())) %>% 
+                  as.numeric() %>% 
+                  abs())) %>% 
   filter(!(
     LONG_DEG < -90 | LONG_DEG > 90 | is.na(LONG_DEG) |
       LONG_MIN < 0 | LONG_MIN >= 60 | is.na(LONG_MIN) |
@@ -125,14 +129,10 @@ l1 %>%
       LAT_DEG < -90 | LAT_DEG > 90 | is.na(LAT_DEG) |
       LAT_MIN < 0 | LAT_MIN >= 60 | is.na(LAT_MIN) |
       LAT_SEC < 0 | LAT_SEC >= 60 | is.na(LAT_SEC))) %>% 
-  mutate(lon = if_else(LONG_DEG < 0, 
-                       LONG_DEG - LONG_MIN / 60 - LONG_SEC / 60^2, 
-                       LONG_DEG + LONG_MIN / 60 + LONG_SEC / 60^2),
-         lat = if_else(LAT_DEG < 0, 
-                       LAT_DEG - LAT_MIN / 60 - LAT_SEC / 60^2, 
-                       LAT_DEG + LAT_MIN / 60 + LAT_SEC / 60^2), 
-         raw_lon = str_c(LONG_DEG, LONG_MIN, LONG_SEC, sep = ":"),
-         raw_lat = str_c(LAT_DEG, LAT_MIN, LAT_SEC, sep = ":")) %>%
+  mutate(lon = dir_lon * (LONG_DEG + LONG_MIN / 60 + LONG_SEC / 60^2),
+         lat = dir_lat * (LAT_DEG + LAT_MIN / 60 + LAT_SEC / 60^2), 
+         raw_lon = str_c(dir_lon, LONG_DEG, LONG_MIN, LONG_SEC, sep = ":"),
+         raw_lat = str_c(dir_lat, LAT_DEG, LAT_MIN, LAT_SEC, sep = ":")) %>%
   select(obs, datetime, ring = BAGUE,
          sp = ESPECE, 
          lon, lat,
@@ -148,18 +148,17 @@ l1 %>%
          !is.na(`N° BAGUE`)) %>% 
   mutate(datetime = str_c(date(DATE), "T12:00") %>% 
            ymd_hm(tz = "Europe/Paris"),
+         dir_lon = if_else(LONG_DEG %>% str_detect("-"), -1, 1),
+         dir_lat = if_else(LAT_DEG %>% str_detect("-"), -1, 1),
          across(c(LONG_DEG, LONG_MIN, LONG_SEC, LAT_DEG, LAT_MIN, LAT_SEC), 
                 ~ .x %>% 
                   str_replace(",|:|;", ".") %>%
-                  as.numeric())) %>% 
-  mutate(lon = if_else(LONG_DEG < 0, 
-                       LONG_DEG - LONG_MIN / 60 - LONG_SEC / 60^2, 
-                       LONG_DEG + LONG_MIN / 60 + LONG_SEC / 60^2),
-         lat = if_else(LAT_DEG < 0, 
-                       LAT_DEG - LAT_MIN / 60 - LAT_SEC / 60^2, 
-                       LAT_DEG + LAT_MIN / 60 + LAT_SEC / 60^2), 
-         raw_lon = str_c(LONG_DEG, LONG_MIN, LONG_SEC, sep = ":"),
-         raw_lat = str_c(LAT_DEG, LAT_MIN, LAT_SEC, sep = ":")) %>% 
+                  as.numeric() %>% 
+                  abs()),
+         lon = dir_lon * (LONG_DEG + LONG_MIN / 60 + LONG_SEC / 60^2),
+         lat = dir_lat * (LAT_DEG + LAT_MIN / 60 + LAT_SEC / 60^2), 
+         raw_lon = str_c(dir_lon, LONG_DEG, LONG_MIN, LONG_SEC, sep = ":"),
+         raw_lat = str_c(dir_lat, LAT_DEG, LAT_MIN, LAT_SEC, sep = ":")) %>% 
   select(obs = ACTION, datetime, ring = `N° BAGUE`,
          sp = ESPECE, 
          lon, lat, co = PAYS, dpt = DEPARTEMENT, com = COMMUNE, spot = LIEUDIT,
@@ -243,7 +242,6 @@ l2 %>%
   pluck(4) %>% 
   filter(ring %in% (l3[[1]] %>% pull(ring))) -> l3[[4]]
 
-
 # Fusion des controles avec les coordonnées -----------------------------------------
 
 list() -> l4
@@ -298,4 +296,157 @@ anti_join(l4 %>% pluck(1), l4 %>% pluck(2), by = "ring") %>%
 
 # Coordinates check by mapping ------------------------------------------------------
 
-l4
+l4 %>% 
+  pluck(2) %>% 
+  filter(!is.na(lon), !is.na(lat)) %>% 
+  distinct(lon, lat, com, spot) %>% 
+  st_as_sf(coords = c("lon", "lat"), crs = 4326) %>% 
+  arrange(com, spot) -> posi
+
+
+leaflet() %>%
+  setView(lng = 3.4, lat = 47, zoom = 6) %>%
+  addProviderTiles(providers$Esri.WorldGrayCanvas,
+                   group = "Fond clair") %>% 
+  addProviderTiles(providers$CartoDB.DarkMatter,
+                   group = "Fond noir") %>% 
+  addProviderTiles(providers$GeoportailFrance.orthos,
+                   group = "Fond satellite") %>% 
+  addProviderTiles(providers$OpenTopoMap,
+                   group = "Fond topographie") -> map_base
+
+# ajout des données par groupes + les popups
+map_base %>% 
+    addCircleMarkers(
+      data = posi,
+      radius = ~ 15 * log10(1 + 1),
+      fillColor = "#EE6352",
+      fillOpacity = 0.7,
+      stroke = TRUE,
+      color = "#EE6352",
+      opacity = 0.7,
+      weight = 2,
+      label = ~ com) %>%
+  addLayersControl(position = "topleft",
+                   baseGroups = c("Fond clair",
+                                  "Fond noir", 
+                                  "Fond satellite",
+                                  "Fond topographie"),
+                   overlayGroups = unique(posi$com),
+                   options = layersControlOptions(collapsed = FALSE)) -> map_1
+
+withr::with_dir('Output', saveWidget(widget = map_1, 
+                                  file = "position_com.html",
+                                  selfcontained = TRUE))
+
+
+# Coordinates checking by country ----------------------------------------------------
+
+st_read("../../LIFE_Donnees/France_shp/countries") -> cos
+
+cos %>% 
+  st_drop_geometry() %>% 
+  pull(name) %>% 
+  str_to_upper() %>% 
+  sort() -> cos2
+
+l4 %>% 
+  map(~ .x %>% 
+        rowid_to_column() %>% 
+        mutate(id = str_c(rowid, ".", obs)) %>% 
+        select(id, co, dpt, com, spot, lon, lat)) %>% 
+  reduce(bind_rows) %>% 
+  filter(!is.na(lon), !is.na(lat)) %>% 
+  left_join(l1 %>% pluck()) -> spots
+
+spots %>% 
+  count(co) %>% 
+  view()
+
+l4 %>% 
+  map(~ .x %>% 
+        rowid_to_column() %>% 
+        mutate(id = str_c(rowid, ".", obs), 
+               co = co %>% 
+                 iconv(from = "UTF-8", to = "ASCII//TRANSLIT") %>% # remove accent
+                 str_to_upper(), 
+               co = case_when(
+                   co %>% str_detect("0000") | is.na(co) ~ "FRANCE", 
+                   co %>% str_detect("BIELORUSSIA") ~ "BELARUS",
+                   co %>% str_starts("BELG") ~ "BELGIUM",
+                   co == "GRECE" ~ "GREECE",
+                   co  %>% str_starts("NETHER") ~ "NETHERLANDS",
+                   co  %>% str_starts("RUSSIA") ~ "RUSSIAN FEDERATION",
+                   co %>% str_starts(c("SWI|SUI|SWT")) ~ "SWITZERLAND",
+                   co %in% c("UK", "OK", "SCOTLAND") ~ "U.K. OF GREAT BRITAIN AND NORTHERN IRELAND",
+                   co == "ITALIA" ~ "ITALY", 
+                   TRUE ~ co)) %>% 
+        select(id, co, dpt, com, spot, lon, lat)) %>% 
+  reduce(bind_rows) -> spots
+
+# how many spots without coordinates
+spots %>% 
+  mutate(
+  pos = if_else(is.na(lon) | is.na(lat), 0, 1)) %>% 
+  count(co, com, spot, pos) %>% 
+  group_by(co, com, spot) %>% 
+  summarize(across(c(pos, n), sum)) %>% 
+  filter(pos == 0) -> no_pos; no_pos
+
+spots %>% 
+  mutate(locali = str_c(com, spot)) %>% 
+  filter(locali %in% c(no_pos %>% mutate(loc = str_c(com, spot)) %>% pull(loc))) %>% 
+  pull(id) -> id_no_pos
+
+# does it fall in the countries? 
+spots %>% 
+  filter(!is.na(lon), !is.na(lat)) %>% 
+  left_join(cos %>% mutate(co = name %>% str_to_upper()) %>% select(co), by = "co") %>% 
+  rowwise() %>% 
+  mutate(geom2 = c(lon, lat) %>% st_point() %>% st_sfc(crs = 4326)) %>% 
+  ungroup() -> spots2
+
+spots2 %>% 
+  rowwise() %>% 
+  mutate(dist = st_distance(geom2, geometry)) %>% 
+  ungroup() -> spot3
+
+spot3 %>% 
+  select(id, co, com, spot, geom2, dist) %>% 
+  mutate(across(dist, ~ .x %>% as.numeric() %>% "*"(10^-3) %>% round()),
+         spot_full = str_c(id, co, com, spot, sep = " / ")) %>% 
+  filter(dist > 0) %>% 
+  arrange(co, com, spot, desc(dist)) -> spot4; spot4
+
+spot4 %>% 
+  mutate(id_co = co %>% as_factor() %>% as.numeric(),
+         colo = hcl(h = seq(15, 300, length = max(id_co)), l = 65, c = 100)[id_co]) %>% 
+  st_as_sf() -> spot5
+
+map_base %>% 
+  addCircleMarkers(
+    data = spot5,
+    group = ~ co,
+    radius = ~ 15 * log10(1 + 1),
+    fillColor = ~ colo,
+    fillOpacity = 0.7,
+    stroke = TRUE,
+    color = "#696773",
+    opacity = 0.7,
+    weight = 2,
+    label = ~ spot_full) %>%
+  addLayersControl(position = "topleft",
+                   baseGroups = c("Fond clair",
+                                  "Fond noir", 
+                                  "Fond satellite",
+                                  "Fond topographie"),
+                   overlayGroups = unique(spot4$co),
+                   options = layersControlOptions(collapsed = FALSE)) %>% 
+  addLegend(data = spot5 %>% 
+              st_drop_geometry() %>% 
+              distinct(co, colo),
+            position = "topright",
+            labels = ~ co,
+            colors = ~ colo, 
+            title = NULL, 
+            opacity = 0.7) -> map_2; map2
