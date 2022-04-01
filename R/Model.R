0ad054c92957a0ee4aba3583908ed08bde3a350f
TABLEAU Adrien <adrien.tableau@oncfs.intra>
2022-04-01 17:03:10 +0200

    update

--- R/Model.R
+++ R/Model.R
@@ -4,6 +4,7 @@ require(tidyverse)
 require(readxl)
 require(lubridate)
 require(sf)
+require(geosphere) # function st_azimuth()
 require(leaflet)
 require(htmlwidgets)
 
@@ -43,6 +44,32 @@ jeu %>%
 
 # Winter spot selection -------------------------------------------------------------
 
+tp1 %>% 
+  mutate(winter = if_else(month(datetime) < 7, -1, 0) + year(datetime),
+         wintering = (ymd(str_c(winter, "1224")) - date(datetime)) %>% as.numeric() %>% abs()) %>% 
+  filter(wintering < 40) %>% 
+  count(ring, winter)
 
 tp1 %>% 
+  mutate(winter = if_else(month(datetime) < 7, -1, 0) + year(datetime),
+         wintering = (ymd(str_c(winter, "1224")) - date(datetime)) %>% as.numeric() %>% abs()) %>% 
+  filter(wintering < 40) %>% 
+  arrange(wintering) %>% 
+  distinct(ring, winter, .keep_all = TRUE) %>% 
+  select(-wintering) %>% 
+  left_join(ring_sel %>% 
+              select(ring, datetime_c = datetime, lon_c = lon, lat_c = lat, origin)) %>% 
+  mutate(angle = atan2(lat - lat_c, lon - lon_c) * 180/pi) %>% 
+  st_as_sf(coords = c("lon", "lat"), crs = 4326) %>% 
+  rowwise() %>% 
+  mutate(geom = st_sfc(st_point(c(lon_c, lat_c)), crs = 4326), 
+         dist = st_distance(geom, geometry, by_element = TRUE) %>% as.numeric() %>% "*"(1e-3),
+         age = case_when(
+           age %in% c("1A", "2A", "pull") & winter == year(datetime_c) ~ "je",
+           TRUE ~ "ad")) %>% 
+  ungroup() -> tmp; tmp
+  
+  tmp %>% 
+    ggplot(aes(x = angle, y = dist)) +
+    geom_point()
   
