159dcbc57ccf49711e10c709d7d87999dd933faa
TABLEAU Adrien <adrien.tableau@oncfs.intra>
2022-04-07 11:01:25 +0200

    favorable waterbodies

--- R/Model.R
+++ R/Model.R
@@ -4,6 +4,8 @@ require(tidyverse)
 require(readxl)
 require(lubridate)
 require(sf)
+require(lwgeom)
+require(units)
 require(leaflet)
 require(htmlwidgets)
 require(stars)
@@ -41,21 +43,17 @@ jeu %>%
       factor(levels = c("repro", "mue", "post-mue", "départ_hivernage", "hivernage", "départ_repro"))) %>% 
   inner_join(ring_sel %>% 
                filter(nobs > 1) %>% 
-               distinct(ring, sp, sex, age, dpt, origin)) -> tp1
+               distinct(ring, sp, sex, age, dpt, origin)) -> cont
 
 # Winter spot selection -------------------------------------------------------------
 
-tp1 %>% 
+cont %>% 
   mutate(winter = if_else(month(datetime) < 7, -1, 0) + year(datetime),
          wintering = (ymd(str_c(winter, "1224")) - date(datetime)) %>% as.numeric() %>% abs()) %>% 
-  filter(wintering < 40) %>% 
-  count(ring, winter)
+  filter(wintering < 40) -> cont2
 
-tp1 %>% 
-  mutate(winter = if_else(month(datetime) < 7, -1, 0) + year(datetime),
-         wintering = (ymd(str_c(winter, "1224")) - date(datetime)) %>% as.numeric() %>% abs()) %>% 
-  filter(wintering < 40) %>% 
-  arrange(wintering) %>% 
+cont2 %>% 
+  arrange(ring, winter, wintering) %>% 
   distinct(ring, winter, .keep_all = TRUE) %>% 
   select(-wintering) %>% 
   left_join(ring_sel %>% 
@@ -68,46 +66,126 @@ tp1 %>%
          age = case_when(
            age %in% c("1A", "2A", "pull") & winter == year(datetime_c) ~ "je",
            TRUE ~ "ad")) %>% 
-  ungroup() -> tmp; tmp
+  ungroup() -> cont3; cont3
 
-tmp %>% 
-  filter(dist > 10, dist < 1500, origin == "West") %>% 
+
+# Effort par pays -------------------------------------------------------------------
+
+cont3 %>% 
+  st_drop_geometry() %>% 
+  left_join(
+    jeu %>% 
+      select(id, co)) %>%
+  mutate(wintering_spot = TRUE) %>% 
+  select(ring, winter, co, age, wintering_spot) %>% 
+  right_join(
+    cont2 %>% 
+      select(-age) %>% 
+      left_join(
+        jeu %>% 
+          select(id, co))) %>% 
+  filter(wintering_spot == TRUE) %>% 
+  count(ring, winter, co, sp, sex, age) %>% 
+  group_by(co) %>% 
+  summarize(nobs_win = mean(n)) %>% 
+  arrange(desc(nobs_win)) -> obs_pressure
+
+cont3 %>% 
+  left_join(
+    jeu %>% 
+      select(id, co)) %>% 
+  left_join(obs_pressure %>% mutate(wgt = 1 / nobs_win)) %>%
+  select(-nobs_win) -> cont4
+
+# direction & angle -----------------------------------------------------------------
+
+cont4 %>% 
+  filter(dist > 10) %>% 
+  ggplot(aes(x = dist, color = sp, group = sp, fill = sp)) + 
+  geom_density(aes(weight = wgt), alpha = .3) +
+  facet_wrap( ~ sp)
+# en moyenne les oiseaux se distribuent selon une loi gamma
+
+cont4 %>% 
+  filter(dist > 10) %>% 
   ggplot(aes(x = angle, y = dist)) +
   geom_point() +
   geom_smooth(method = "gam") + 
-  coord_polar(start = pi/2, direction = -1)
+  coord_polar(start = pi/2, direction = -1) +
+  ylim(c(0, 1500)) +
+  scale_x_continuous(limits = c(-180, 180), 
+                     breaks = seq(-90, 180, length.out = 4),
+                     labels = c("S", "E", "N", "W"))
+# -> les oiseaux vont plus loin direction sud ouest
 
-
-tmp %>% 
-  filter(dist > 50) %>% 
-  ggplot(aes(x = dist, color = sp, group = sp, fill = sp)) + 
-  geom_density() +
-  facet_wrap( ~ sp)
-
-tmp %>% 
-  filter(dist > 350, origin == "West") %>% 
+cont4 %>% 
+  filter(dist > 10, origin == "West") %>% 
   ggplot(aes(x = angle, color = sp, group = sp, fill = sp)) + 
   # geom_histogram(binwidth = 22.5, boundary = -180, alpha = .3) +
-  geom_density(alpha = .3) +
+  geom_density(alpha = .3, aes(weight = wgt)) +
   coord_polar(direction = - 1, start = pi/2) +
   scale_x_continuous(limits = c(-180, 180), 
                      breaks = seq(-90, 180, length.out = 4),
                      labels = c("S", "E", "N", "W")) +
-  facet_wrap(~sp)
-
-
+  facet_wrap( ~ sp)
 
 # Coline Land Cover ----------------------------------------------------------------
 
-read_stars("./Data/clc2018/DATA/U2018_CLC2018_V2020_20u1.tif") -> clc
+st_read("./Data/Coline_Land_Cover_2018/clc.shp", as_tibble = TRUE) -> clc
 
-st_read("./Data/clc2018_poly2/u2018_clc2018_v2020_20u1_geoPackage/DATA/U2018_CLC2018_V2020_20u1.gpkg",
-        query = 'SELECT * FROM "U2018_CLC2018_V2020_20u1" WHERE FID = 1') -> clcp1
+clc %>% 
+  st_union() %>% 
+  st_cast("POLYGON") -> part
 
-st_read("./Data/clc2018_poly/DATA/U2018_CLC2018_V2020_20u1.gdb", 
-        query = 'SELECT * FROM "U2018_CLC2018_V2020_20u1" WHERE FID = 1') -> clcp2
+part %>% 
+  st_as_sf() %>%
+  rename(geometry = x) %>% 
+  mutate(st_centroid(part) %>% st_as_sf() %>% st_transform(4326) %>% rename(centroid = x),
+         area = st_area(.),
+         perimeter = st_perimeter(.)) %>% 
+  st_transform(4326) %>% 
+  rowid_to_column() -> water
 
-st_read("./Data/clc2018_poly3/clc.shp") -> clc3
+water %>% 
+  st_filter(cont4, .predicate = st_contains) -> occupied
 
-plot(clc3)
+water %>% 
+  filter(drop_units(area) < 25e5) %>% 
+  left_join(occupied %>% distinct(rowid) %>% mutate(occupied = TRUE)) %>% 
+  mutate(across(occupied, replace_na, FALSE)) %>% 
+  ggplot(aes(x = area, fill = occupied, group = occupied)) + 
+  geom_histogram() + 
+  facet_wrap(~ occupied, scales = "free_y")
 
+water %>% 
+  left_join(occupied %>% distinct(rowid) %>% mutate(occupied = TRUE)) %>% 
+  mutate(across(occupied, replace_na, FALSE)) %>% 
+  filter(occupied == TRUE) %>% 
+  pull(area) %>% 
+  quantile(probs = seq(0, 1, .1)) %>% 
+  bind_rows(
+    water %>% 
+      pull(area) %>% 
+      quantile(probs = seq(0, 1, .1)))
+
+# weighting water bodies ------------------------------------------------------------
+
+water %>% 
+  left_join(
+    occupied %>% 
+      as_tibble() %>% 
+      mutate(occupied = TRUE)) %>% 
+  mutate(across(occupied, replace_na, FALSE)) %>% 
+  # filter(occupied == TRUE) %>% 
+  mutate(across(area, drop_units)) %>% 
+  glm(area ~ 1, data = ., family = Gamma()) %>% residuals() %>% qqnorm()
+
+water %>% 
+  left_join(occupied %>% distinct(rowid) %>% mutate(occupied = TRUE)) %>% 
+  mutate(across(occupied, replace_na, FALSE)) %>% 
+  filter(occupied == TRUE) %>% 
+  pull(area) %>% 
+  quantile(probs = seq(0, 1, .05)) %>% 
+  as_tibble() %>% 
+  mutate(qt = seq(0, 1, .05), 
+         diff = c(0, diff(value)))
